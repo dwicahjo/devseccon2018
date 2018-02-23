a80f15dfdbcb87e0ab1d88481beba4a16cf793e8
Dwi Cahyo <dwi.prasetyo@ruma.co.id>
2018-02-23 13:04:41 +0700

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://admin:admin@ds145359.mlab.com:45359/mongo_cjo
+#export MONGODB_CREDENTIALS=mongodb://admin:admin@ds145359.mlab.com:45359/mongo_cjo
 
 # finally, run the app in dev mode...
 npm run dev
